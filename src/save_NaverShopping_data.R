3fa7ee6bc2557e1c565a3fd33a022355d4d49843
KeunYoung Park <superneo77@gmail.com>
2020-08-20 23:59:51 +0900

    feat: Initiate a script to save Naver Shopping Insight API data

---
+++ src/save_NaverShopping_data.R
@@ -0,0 +1,23 @@
+# save_NaverShopping_data.R
+#
+# Purpose: Save Naver Shopping Insight API data
+#
+# Author: KeunYoung Park
+# License: MIT License
+#
+# Date: 2020-08
+#
+# Version: 1.0
+# Version history:
+#   1.0  initial version
+#
+# ==============================================================================
+
+library(here)
+
+Naver_Cat_ID <- "50000804"  # blouse/shirts
+url <- "https://openapi.naver.com/v1/datalab/shopping/category/device"
+doc <- list(startDate = NULL, endDate = NULL, timeUnit = "date",
+            category = Naver_Cat_ID)
+
+cout("Test if it works")
